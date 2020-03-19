0d66950a4f7bf391ef51c33f4ad992779989fe64
Andrew Kachnic <ajkachnic@protonmail.com>
2020-03-19 08:46:07 -0400

    Updated UI

--- pages/index.js
+++ pages/index.js
@@ -50,7 +50,6 @@ const Home = () => {
         <input value={url} onChange={val => setUrl(val.target.value)} placeholder="URL To Shorten"/>
         <button type="submit">Shorten</button>
       </form>
-    </main>
     <h1 className="title">
       Check A URL
     </h1>
@@ -60,6 +59,7 @@ const Home = () => {
         <input value={keyToCheck} onChange={val => setKeyToCheck(val.target.value)} placeholder="Key ..."/>
         <button type="submit">Shorten</button>
       </form>
+    </main>
     <style jsx>{`
       .container {
         min-height: 100vh;
@@ -87,7 +87,9 @@ const Home = () => {
         justify-content: center;
         align-items: center;
       }
-
+      form {
+        margin: 4rem;
+      }
       footer img {
         margin-left: 0.5rem;
       }
@@ -117,7 +119,7 @@ const Home = () => {
       .title {
         margin: 0;
         line-height: 1.15;
-        font-size: 4rem;
+        font-size: 2.5rem;
       }
 
       .title,
@@ -130,60 +132,27 @@ const Home = () => {
         font-size: 1.5rem;
       }
 
-      code {
-        background: #fafafa;
-        border-radius: 5px;
-        padding: 0.75rem;
-        font-size: 1.1rem;
-        font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-          DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-      }
-
-      .grid {
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        flex-wrap: wrap;
-
-        max-width: 800px;
-        margin-top: 3rem;
-      }
-
-      .card {
-        margin: 1rem;
-        flex-basis: 45%;
-        padding: 1.5rem;
-        text-align: left;
-        color: inherit;
-        text-decoration: none;
-        border: 1px solid #eaeaea;
-        border-radius: 10px;
-        transition: color 0.15s ease, border-color 0.15s ease;
-      }
-
-      .card:hover,
-      .card:focus,
-      .card:active {
-        color: #0070f3;
-        border-color: #0070f3;
-      }
-
-      .card h3 {
-        margin: 0 0 1rem 0;
-        font-size: 1.5rem;
-      }
-
-      .card p {
-        margin: 0;
-        font-size: 1.25rem;
-        line-height: 1.5;
-      }
-
       @media (max-width: 600px) {
         .grid {
           width: 100%;
           flex-direction: column;
         }
+      }
+      input {
+        padding:1rem;
+        border:none;
+        box-shadow: 0px 0px 5px rgba(0, 0, 0, .25);
+        margin-right:1rem;
+
+        font-size:1.25rem;
+      }
+      button {
+        padding:1rem 1rem;
+        color:white;
+        background: #151515;
+        font-weight: 700;
+        border:none;
+        font-size:1.25rem;
       }
     `}</style>
 
