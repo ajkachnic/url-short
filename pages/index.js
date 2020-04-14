9aa897de5c25965c398a585343d2768f66504b9c
Andrew Kachnic <ajkachnic@protonmail.com>
2020-04-14 18:47:44 -0400

    Added URL

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ import { useRouter } from 'next/router'
 const Home = () => {
   const router = useRouter();
   const [url, setUrl] = useState('')
-  const [message, setMessage] = useState('')
+  const [link, setLink] = useState('')
   const [lookupMessage, setLookupMessage] = useState('')
   const [keyToCheck, setKeyToCheck] = useState('')
   const createURL = (event) => {
@@ -14,7 +14,7 @@ const Home = () => {
       redirectUrl: url
     })
     .then(data => {
-      setMessage(`The URL is ${process.env.SERVER_URL}/${data.data.linkId}`)
+      setLink(`${process.env.SERVER_URL}/${data.data.linkId}`)
     })
     .catch(err => console.log(err))
   }
@@ -35,141 +35,157 @@ const Home = () => {
     })
   }
   return (
-  <div className="container">
-    <Head>
-      <title>Short A URL</title>
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
+    <div className="container">
+      <Head>
+        <title>Short A URL</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
-    <main>
-      <h1 className="title">
-        Shorten A URL
-      </h1>
-      <p>{message}</p>
-      <form onSubmit={createURL}>
-        <input value={url} onChange={val => setUrl(val.target.value)} placeholder="URL To Shorten"/>
-        <button type="submit">Shorten</button>
-      </form>
-    <h1 className="title">
-      Check A URL
-    </h1>
+      <main>
+        <h1 className="title">Shorten A URL</h1>
+        {link ? "The URL is " : ""}
+        {link ? (
+          <a href={link} target="_blank" className="link">
+            {link}
+          </a>
+        ) : (
+          ""
+        )}
+        <form onSubmit={createURL}>
+          <input
+            value={url}
+            onChange={(val) => setUrl(val.target.value)}
+            placeholder="URL To Shorten"
+          />
+          <button type="submit">Shorten</button>
+        </form>
+        <h1 className="title">Check A URL</h1>
 
-    <p>{lookupMessage}</p>
-    <form onSubmit={checkURL}>
-        <input value={keyToCheck} onChange={val => setKeyToCheck(val.target.value)} placeholder="Key ..."/>
-        <button type="submit">Shorten</button>
-      </form>
-    </main>
-    <style jsx>{`
-      .container {
-        min-height: 100vh;
-        padding: 0 0.5rem;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-      }
-
-      main {
-        padding: 5rem 0;
-        flex: 1;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-      }
-
-      footer {
-        width: 100%;
-        height: 100px;
-        border-top: 1px solid #eaeaea;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-      }
-      form {
-        margin: 4rem;
-      }
-      footer img {
-        margin-left: 0.5rem;
-      }
-
-      footer a {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-      }
-
-      a {
-        color: inherit;
-        text-decoration: none;
-      }
-
-      .title a {
-        color: #0070f3;
-        text-decoration: none;
-      }
-
-      .title a:hover,
-      .title a:focus,
-      .title a:active {
-        text-decoration: underline;
-      }
-
-      .title {
-        margin: 0;
-        line-height: 1.15;
-        font-size: 2.5rem;
-      }
-
-      .title,
-      .description {
-        text-align: center;
-      }
-
-      .description {
-        line-height: 1.5;
-        font-size: 1.5rem;
-      }
-
-      @media (max-width: 600px) {
-        .grid {
-          width: 100%;
+        {link ? "The URL is" : ""}
+        <form onSubmit={checkURL}>
+          <input
+            value={keyToCheck}
+            onChange={(val) => setKeyToCheck(val.target.value)}
+            placeholder="Key ..."
+          />
+          <button type="submit">Shorten</button>
+        </form>
+      </main>
+      <style jsx>{`
+        .container {
+          min-height: 100vh;
+          padding: 0 0.5rem;
+          display: flex;
           flex-direction: column;
+          justify-content: center;
+          align-items: center;
         }
-      }
-      input {
-        padding:1rem;
-        border:none;
-        box-shadow: 0px 0px 5px rgba(0, 0, 0, .25);
-        margin-right:1rem;
 
-        font-size:1.25rem;
-      }
-      button {
-        padding:1rem 1rem;
-        color:white;
-        background: #151515;
-        font-weight: 700;
-        border:none;
-        font-size:1.25rem;
-      }
-    `}</style>
+        main {
+          padding: 5rem 0;
+          flex: 1;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+        }
+        a.link {
+          color: #5050fa;
+          text-decoration: none;
+          font-weight: 700;
+        }
+        footer {
+          width: 100%;
+          height: 100px;
+          border-top: 1px solid #eaeaea;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        }
+        form {
+          margin: 4rem;
+        }
+        footer img {
+          margin-left: 0.5rem;
+        }
 
-    <style jsx global>{`
-      html,
-      body {
-        padding: 0;
-        margin: 0;
-        font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
-          Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
-      }
+        footer a {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        }
 
-      * {
-        box-sizing: border-box;
-      }
-    `}</style>
-  </div>
-)
+        a {
+          color: inherit;
+          text-decoration: none;
+        }
+
+        .title a {
+          color: #0070f3;
+          text-decoration: none;
+        }
+
+        .title a:hover,
+        .title a:focus,
+        .title a:active {
+          text-decoration: underline;
+        }
+
+        .title {
+          margin: 0;
+          line-height: 1.15;
+          font-size: 2.5rem;
+        }
+
+        .title,
+        .description {
+          text-align: center;
+        }
+
+        .description {
+          line-height: 1.5;
+          font-size: 1.5rem;
+        }
+
+        @media (max-width: 600px) {
+          .grid {
+            width: 100%;
+            flex-direction: column;
+          }
+        }
+        input {
+          padding: 1rem;
+          border: none;
+          box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.25);
+          margin-right: 1rem;
+
+          font-size: 1.25rem;
+        }
+        button {
+          padding: 1rem 1rem;
+          color: white;
+          background: #151515;
+          font-weight: 700;
+          border: none;
+          font-size: 1.25rem;
+        }
+      `}</style>
+
+      <style jsx global>{`
+        html,
+        body {
+          padding: 0;
+          margin: 0;
+          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+            sans-serif;
+        }
+
+        * {
+          box-sizing: border-box;
+        }
+      `}</style>
+    </div>
+  );
     }
 export default Home
